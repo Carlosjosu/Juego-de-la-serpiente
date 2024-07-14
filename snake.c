bf0a027af74ad7bfbe76248a23b56e26e4339649
Carlosjosu <carlos.j.granda.c@unl.edu.ec>
2024-07-14 15:49:49 -0500

    Add files via upload

--- snake.c
+++ snake.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+#define UP 1
+#define DOWN 2
 #define V 21
 #define H 65
 #define N 100
@@ -9,25 +13,51 @@ typedef struct
     int x, y;
     int Modx, Mody;
     char imagen;
-} serpiente;
+} snk;
 
 typedef struct
 {
     int x, y;
 } fruta;
 
-serpiente snake[N];
-
+snk snake[N];
 fruta fruta_actual;
 
-int main()
+void Inicio_Campo(char campo[V][H])
 {
-    int tamaño;
-    char campo[V][H];
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < H; j++)
+        {
+            if (i == 0 || i == V - 1 || j == 0 || j == H - 1)
+            {
+                campo[i][j] = '#';
+            }
+            else
+            {
+                campo[i][j] = ' ';
+            }
+        }
+    }
+}
 
-    inicio(&tamaño, campo);
+void Inicio_Datos(char campo[V][H], int tamaño)
+{
+    for (int i = 1; i < tamaño; i++)
+    {
+        snake[i].x = snake[i - 1].x - 1;
+        snake[i].y = snake[i - 1].y;
 
-    return 0;
+        snake[i].imagen = 'X';
+    }
+
+    snake[0].imagen = 'O';
+
+    for (int i = 0; i < tamaño; i++)
+    {
+        campo[snake[i].y][snake[i].x] = snake[i].imagen;
+    }
+    campo[fruta_actual.y][fruta_actual.x] = '*';
 }
 
 void inicio(int *tamaño, char campo[V][H])
@@ -54,4 +84,39 @@ void inicio(int *tamaño, char campo[V][H])
     {
         fruta_actual.y = rand() % (V - 1);
     }
+
+    for (int i = 0; i < *tamaño; i++)
+    {
+        snake[i].Modx = 1;
+        snake[i].Mody = 0;
+    }
+
+    Inicio_Campo(campo);
+    Inicio_Datos(campo, *tamaño);
+}
+
+// Se crea el campo de juego
+
+void dibujar(char campo[V][H])
+{
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < H; j++)
+        {
+            printf("%c", campo[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int tamaño;
+    char campo[V][H];
+
+    inicio(&tamaño, campo);
+    dibujar(campo);
+
+    system("pause");
+    return 0;
 }
